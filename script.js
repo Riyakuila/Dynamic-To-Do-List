3a0dc89be523add866edb756ab5b8ee7280c2e37
Riya Kuila <160008942+Riyakuila@users.noreply.github.com>
2024-08-18 15:21:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("inputBox");
+const listContaainer = document.getElementById("listContainer");
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContaainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContaainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContaainer.innerHTML);
+}
+
+function showTask(){
+    listContaainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
